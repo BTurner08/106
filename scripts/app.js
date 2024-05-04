9617cdd18ab8a216fb2eeddf9f1b64446c9a8b2b
BTurner08 <mrbturner2013@yahoo.com>
2024-05-04 15:06:09 -0400

    106 1

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+function sayHello(){
+    console.log("Hello there")
+}
+function sayGoodBye(name,lastName){
+    console.log("Adios! "+name+" "+lastName)
+    var color = "Blue"
+}
+function init() {
+    console.log("hello world")
+    // if the users pays they can visit
+    // {
+    sayHello();
+    let name = "Blake"
+    sayGoodBye(name, "Turner")
+    // }
+}
+window.onload = init; //avoid the init()
