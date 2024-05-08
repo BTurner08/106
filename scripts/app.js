d9010c34d5f7a9d799981041efda786d5df4ec89
BTurner08 <mrbturner2013@yahoo.com>
2024-05-07 20:55:35 -0400

    106 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,62 @@
-function sayHello(){
-    console.log("Hello there")
+function saveTask(){
+    console.log("Saving task");
+    //get value
+    const title =$("#txtTitle").val();
+    const desc =$("#txtDescription").val();
+    const color =$("#selColor").val();
+    const date =$("#selDate").val();
+    const status =$("#selStatus").val();
+    const budget =$("#numBudget").val();
+    console.log(title,desc,color,date,status,budget)
+    //build object
+    let taskToSave = new Task (title,desc,color,date,status,budget);
+    console.log(taskToSave);
+    displayTask(taskToSave);
+    //save to server
+
+    //display the 
+    function displayTask(task){
+        let syntax = `<div class ="task" style="border-color:${task.color}">
+        <div class="info">
+        <h3>${task.title}</h3>
+        <p>${task.desc}</p>
+        </div>
+        <label class="status">${task.status}</label>
+        <div class="date-budget">
+            <label>${task.date}</label>
+            <label>${task.budget}</label>
+        </div>
+        </div>
+        `;
+        $(".pending-task").append(syntax)
+    }
+    if (!title || !desc || !color || !date || !status || !budget) {
+        alert("Please fill in all fields.");
+        return;
 }
-function sayGoodBye(name,lastName){
-    console.log("Adios! "+name+" "+lastName)
-    var color = "Blue"
 }
+
+function testRequest(){
+
+    $.ajax({
+        type: "GET",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    });
+}
+
 function init() {
-    console.log("hello world")
-    // if the users pays they can visit
-    // {
-    sayHello();
-    let name = "Blake"
-    sayGoodBye(name, "Turner")
-    // }
+
+    //load data
+    
+    //hook events
+    $("#btnSave").click(saveTask)
 }
+
 window.onload = init; //avoid the init()
 //hello
